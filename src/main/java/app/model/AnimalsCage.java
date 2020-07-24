223d8ccdbfc99f9146e9501f2dd418ef5defac7a
georgekhlebnikov <george.khlebnikov@yandex.ru>
2020-07-25 01:29:05 +0300

    [2.1.3] rc1.1 (minor fix)

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -2,6 +2,7 @@ package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
